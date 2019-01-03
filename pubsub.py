e7c0a9b3a9e3a9f728ec32af9fb481d337adf1b7
Federico Martinez <federicoemartinez@gmail.com>
2019-01-03 09:25:04 -0300

    Revert "agrego broker y soporte de registro multiuid"
    This reverts commit 048cf6d15a448282ef7fed06bb9725e6e929e72e.

--- pubsub.py
+++ pubsub.py
@@ -1,15 +1,11 @@
 __author__ = 'Fede M'
 
-import io
-
 from twisted.logger import Logger, textFileLogObserver
+import io
+log = Logger(observer=textFileLogObserver(io.open("pubsub.log",'a')))
 
-
-log = Logger(observer=textFileLogObserver(io.open("pubsub.log", 'a')))
-
-# Code for PyInstaller
+#Code for PyInstaller
 import sys
-
 if 'twisted.internet.reactor' in sys.modules:
     del sys.modules['twisted.internet.reactor']
 
@@ -17,74 +13,60 @@ if 'twisted.internet.reactor' in sys.modules:
 
 try:
     from twisted.internet import epollreactor
-
     epollreactor.install()
 except:
     try:
         from twisted.internet import iocpreactor
-
         iocpreactor.install()
     except:
         from twisted.internet import selectreactor
-
         selectreactor.install()
+
 
 from twisted.internet import reactor, protocol, endpoints
 from twisted.protocols import basic
 import json
 
 
+
 class PubProtocol(basic.LineReceiver):
+
     def __init__(self, factory):
         self.factory = factory
-        self.uids = []
+        self.uid = None
 
 
     def connectionLost(self, reason):
-        self._clean_uids()
-
-    def _clean_uids(self):
-        if self.uids:
-            for each in self.uids:
-                if each in self.factory.clients and self in self.factory.clients[each]:
-                    self.factory.clients[each].remove(self)
-                    if len(self.factory.clients[each]) == 0:
-                        del self.factory.clients[each]
+        if self.uid and self.uid in self.factory.clients and self.factory.clients[self.uid] == self:
+            log.info("removing " + str(self.uid))
+            del self.factory.clients[self.uid]
 
     def lineReceived(self, line):
         log.debug("{line!r}", line=line)
-        #if self.uids == []:
-        try:
-            data = json.loads(line.rstrip())
-            if "to" in data:
-                uid_to = data["to"]
-                clients = self.factory.clients.get(uid_to)
-                if clients:
-                    for client in clients:
-                        try:
-                            client.sendLine(str(uid_to))
-                        except Exception, e:
-                            log.error("{message!r}", message=e.message)
-                            client.transport.loseConnection()
-            elif "uids" in data:
-                    self._clean_uids()
-                    self.uids = data["uids"]
-                    for each in self.uids:
-                        self.factory.clients[each].add(self)
-            else:
-                log.warn("{line!r}", line=line)
-
-        except Exception, e:
-            log.error("{message!r}", message=e.message)
-            log.error("{line!r}", line=line)
-
-
-from collections import defaultdict
+        if self.uid is None:
+            try:
+                data = json.loads(line.rstrip())
+                if "to" in data:
+                    client = self.factory.clients.get(data["to"])
+                    if client: client.sendLine("ping")
+                else:
+                    if "uid" in data:
+                        self.uid = data["uid"]
+                        if self.uid in self.factory.clients:
+                           log.warn("UID {uid} is trying to register but it is already registered", uid=self.uid)
+                           other_client = self.factory.clients[self.uid]
+                           other_client.transport.loseConnection()
+                        self.factory.clients[self.uid] = self
+            except Exception, e:
+                log.error("{message!r}", message = e.message)
+                log.error("{line!r}", line=line)
+        else:
+            log.warn("{line!r}", line=line)
 
 
 class PubFactory(protocol.Factory):
     def __init__(self):
-        self.clients = defaultdict(lambda: set())
+        self.clients = {}
 
     def buildProtocol(self, addr):
         return PubProtocol(self)
